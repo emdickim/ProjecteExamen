5262b0420ff087bf1d5d930b69573d17ef3c99cf
emdickim <kimlazaropardo@gmail.com>
2026-02-03 20:01:37 +0100

    fet el reste

--- app/src/main/java/com/mugiwara/examen/MainActivity.kt
+++ app/src/main/java/com/mugiwara/examen/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.mugiwara.examen
 
+import android.graphics.Insets
+import android.graphics.Insets.add
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //NO COMPILA PER AIXO
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<TasquesFragment>(R.id.)
+            }
         }
 
 
